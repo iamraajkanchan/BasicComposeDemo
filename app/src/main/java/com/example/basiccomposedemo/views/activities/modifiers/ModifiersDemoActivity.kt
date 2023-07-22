d6e64ae3136624a96f78853a2c6db7685c139f43
Raaj Kanchan <iamraajkanchan@gmail.com>
2023-07-22 22:14:52 +0530

    Finished Working on ModifiersDemoActivity

---
+++ app/src/main/java/com/example/basiccomposedemo/views/activities/modifiers/ModifiersDemoActivity.kt
@@ -0,0 +1,84 @@
+package com.example.basiccomposedemo.views.activities.modifiers
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.basiccomposedemo.R
+import com.example.basiccomposedemo.views.activities.rowsAndColumns.RowsAndColumnsBasicsActivity
+
+class ModifiersDemoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(modifier = Modifier.padding(20.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.icon_back_arrow),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .width(30.dp)
+                        .height(30.dp)
+                        .clickable { goToRowsAndColumnsActivity() }
+                )
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(20.dp)
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(
+                            color = colorResource(id = R.color.teal_200),
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                        .fillMaxWidth()
+                        .height(100.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(text = "Hello", color = colorResource(id = R.color.white))
+                    Text(text = "World", color = colorResource(id = R.color.white))
+                    Button(onClick = { goToImageCardDemoActivity() }) {
+                        Text(text = "Go To ImageCard")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun goToRowsAndColumnsActivity() {
+        Intent(this@ModifiersDemoActivity, RowsAndColumnsBasicsActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(this)
+        }
+    }
+
+    private fun goToImageCardDemoActivity() {
+        Toast.makeText(
+            this@ModifiersDemoActivity,
+            "Action not implemented yet!",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+}
--- app/src/main/java/com/example/basiccomposedemo/views/activities/rowsAndColumns/RowsAndColumnsBasicsActivity.kt
+++ app/src/main/java/com/example/basiccomposedemo/views/activities/rowsAndColumns/RowsAndColumnsBasicsActivity.kt
@@ -1,12 +1,17 @@
 package com.example.basiccomposedemo.views.activities.rowsAndColumns
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,16 +23,32 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.basiccomposedemo.R
+import com.example.basiccomposedemo.views.activities.main.MainActivity
+import com.example.basiccomposedemo.views.activities.modifiers.ModifiersDemoActivity
 
 class RowsAndColumnsBasicsActivity : ComponentActivity() {
     private val tealColor = Color(0xFF009688);
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Image(
+                painter = painterResource(id = R.drawable.icon_back_arrow),
+                contentDescription = "Back Arrow",
+                modifier = Modifier
+                    .width(30.dp)
+                    .height(30.dp)
+                    .padding(top = 5.dp, start = 5.dp)
+                    .clickable { goToMainActivity() }
+            )
+            Spacer(modifier = Modifier
+                .height(20.dp)
+                .fillMaxWidth())
             Column(
                 modifier = Modifier
                     .padding(20.dp)
@@ -65,10 +86,15 @@ class RowsAndColumnsBasicsActivity : ComponentActivity() {
     }
 
     private fun goToModifierActivity() {
-        Toast.makeText(
-            this@RowsAndColumnsBasicsActivity,
-            "Not implemented the activity!",
-            Toast.LENGTH_LONG
-        ).show()
+        Intent(this@RowsAndColumnsBasicsActivity, ModifiersDemoActivity::class.java).apply {
+            startActivity(this)
+        }
+    }
+
+    private fun goToMainActivity() {
+        Intent(this@RowsAndColumnsBasicsActivity, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(this)
+        }
     }
 }
